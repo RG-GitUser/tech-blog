9cda58381265143ad7d4033aee4d137d38f8eb05
RG-GitUser <rileygaffney3@icloud.com>
2023-11-19 22:04:26 -0400

    adding dependancies for express

---
+++ utils/bcrypt.js
@@ -0,0 +1,23 @@
+//bcrypt async 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const someOtherPlaintextPassword = 'not_bacon';
+
+
+//to hash password 
+bcrypt.genSalt(saltRounds, function(err, salt) {
+    bcrypt.hash(myPlaintextPassword, salt, function(err, hash) {
+        // Store hash in your password DB.
+    });
+});
+
+
+//to check password
+// Load hash from your password DB.
+bcrypt.compare(myPlaintextPassword, hash, function(err, result) {
+    // result == true
+});
+bcrypt.compare(someOtherPlaintextPassword, hash, function(err, result) {
+    // result == false
+});
