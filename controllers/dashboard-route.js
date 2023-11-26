b59af63d3c6b816431b20256353d74cd072f918a
RG-GitUser <rileygaffney3@icloud.com>
2023-11-26 14:39:22 -0400

    editing routes and added new code for models

--- controllers/dashboard-route.js
+++ controllers/dashboard-route.js
@@ -1,8 +1,32 @@
 const router = require("express").Router();
+const {Post, User}= require('.../models');
+const withAuth = require('utils/auth');
 
-// Define a route for the dashboard
-app.get('/dashboard', (req, res) => {
-    res.sendFile(__dirname + './views/dashboard.hbs');
+
+router.get('/', async (req, res) => {
+    try {
+      // Get all posts and JOIN with user data
+      const PostData = await Post.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ['name'],
+          },
+        ],
+      });
+  
+       // Serialize data so the template can read it
+    const post = postData.map((post => post.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      posts, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
+  
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
+const apiRoutes = require('./api');
 
+router.use('/api', apiRoutes);
 
 module.exports = router;
