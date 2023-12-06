07acc1387f14651c783a47bfa97fef60d2c12567
RG-GitUser <rileygaffney3@icloud.com>
2023-12-05 21:46:56 -0400

    changes to home.js/home.handlebars

--- public/js/home.js
+++ public/js/home.js
@@ -1,41 +1,58 @@
 document.addEventListener("DOMContentLoaded", function () {
-  fetch('./seeds')
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then(data => {
-      var homepageTemplateSource = document.getElementById('homepage-template').innerHTML;
-      var homepageTemplate = Handlebars.compile(homepageTemplateSource);
+  const TEMPLATE_ID = 'homepage-template';
+  const POSTS_CONTAINER_ID = 'posts';
 
-      // Process the blog post data to include comments
-      var blogPostsWithComments = processBlogPostsWithComments(data);
+  // Function to render blog posts
+  function renderBlogPosts(posts) {
+    var homepageTemplateSource = document.getElementById(TEMPLATE_ID).innerHTML;
+    var homepageTemplate = Handlebars.compile(homepageTemplateSource);
 
-      // Render homepage template with the latest blog posts and comments
-      var latestBlogPosts = getLatestBlogPosts(blogPostsWithComments, 3); 
-      var homepageHtml = homepageTemplate({ blogPosts: latestBlogPosts });
-      document.getElementById('posts').innerHTML = homepageHtml;
-    })
-    .catch(error => {
-      console.error('Error fetching blog data:', error);
-    });
-});
+    // Process the blog post data to include comments
+    var blogPostsWithComments = processBlogPostsWithComments(posts);
 
-// Function to process blog post data and include comments
-function processBlogPostsWithComments(allBlogPosts) {
-  return allBlogPosts.map(post => {
+    // Render homepage template with the latest blog posts and comments
+    var latestBlogPosts = getLatestBlogPosts(blogPostsWithComments, 3); 
+    var homepageHtml = homepageTemplate({ blogPosts: latestBlogPosts });
+    document.getElementById(POSTS_CONTAINER_ID).innerHTML = homepageHtml;
+  }
 
-    post.comments = post.comments || [];
-    return post;
+  // Fetch initial blog posts on page load
+  fetchBlogPosts();
+
+  // Function to fetch and render blog posts
+  function fetchBlogPosts() {
+    fetch('./seeds')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Network response was not ok - Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        renderBlogPosts(data);
+      })
+      .catch(error => {
+        console.error('Error fetching blog data:', error);
+      });
+  }
+
+  // Event listener for a new blog post creation
+  document.addEventListener('newPostCreated', function () {
+    // Fetch and render the updated list of blog posts after a new post is created
+    fetchBlogPosts();
   });
-}
 
-// Function to get the latest blog posts
-function getLatestBlogPosts(allBlogPosts, count) {
+  // Function to process blog post data and include comments
+  function processBlogPostsWithComments(allBlogPosts) {
+    return allBlogPosts.map(post => {
+      post.comments = post.comments || [];
+      return post;
+    });
+  }
 
-  const sortedPosts = allBlogPosts.sort((a, b) => new Date(b.dateCreated) - new Date(a.dateCreated));
-  
-  return sortedPosts.slice(0, count);
-}
+  // Function to get the latest blog posts
+  function getLatestBlogPosts(allBlogPosts, count) {
+    const sortedPosts = allBlogPosts.sort((a, b) => new Date(b.dateCreated) - new Date(a.dateCreated));
+    return sortedPosts.slice(0, count);
+  }
+});
