26d26ea755e7f536ce73bddc8f925abcf4e0ba99
RG-GitUser <rileygaffney3@icloud.com>
2023-12-11 20:32:19 -0400

    fixed errors with server

--- controllers/api/authRoutes.js
+++ controllers/api/authRoutes.js
@@ -5,11 +5,9 @@ const authenticate = require('./authMiddleware');
 
 const router = express.Router();
 
-// Your authentication routes go here
+//authenticating where applicable 
 
-// Example route using the authenticate middleware
 router.get('/dashboard', authenticate, (req, res) => {
-  // You can access req.auth.isAuthenticated here
   if (req.auth.isAuthenticated) {
     res.render('dashboard');
   } else {
--- utils/auth.js
+++ utils/auth.js
@@ -1,7 +1,6 @@
-// authMiddleware.js
+const jwt = require('jsonwebtoken');
 
 const authenticate = (req, res, next) => {
-  // Implement your authentication logic here.
   // For example, you can validate a token.
 
   const authToken = req.headers.authorization;
@@ -12,27 +11,22 @@ const authenticate = (req, res, next) => {
     return next();
   }
 
-  // Perform your token validation logic here
-  // Replace this with your actual token validation mechanism
-  if (validateToken(authToken)) {
-    // If token is valid, set isAuthenticated to true
-    req.auth = { isAuthenticated: true };
-  } else {
-    // If token is invalid, set isAuthenticated to false
-    req.auth = { isAuthenticated: false };
-  }
+  const secretKey = 'SECRET-TECH';
 
-  next();
-};
+  // Verify the token using jsonwebtoken
+  jwt.verify(authToken, secretKey, (err, decoded) => {
+    if (err) {
+      // If token fails, user is not authenticated
+      req.auth = { isAuthenticated: false };
+    } else {
+      // If token is valid, set isAuthenticated to true and attach user data
+      req.auth = { isAuthenticated: true, user: decoded.user };
+    }
 
-// Replace this function with your actual token validation logic
-const validateToken = (token) => {
-  // Implement your token validation logic here
-  // For example, you can use a library like jsonwebtoken
-  // or validate the token against your authentication server
-
-  // Replace this with your actual token validation logic
-  return true;
+  
+    next();
+  });
 };
 
 module.exports = authenticate;
+
