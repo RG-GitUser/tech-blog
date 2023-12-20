88d3433c2f796610fe49d25512a96012bfc968f7
RG-GitUser <rileygaffney3@icloud.com>
2023-12-20 16:27:03 -0400

    added modal functionality to delete blogpost

--- public/js/home.js
+++ public/js/home.js
@@ -39,9 +39,57 @@ function updateUI(blogPosts) {
   }
 }
 
-//Delete Post 
-
-
 
  //Delete post functionality 
+$(document).ready(function() {
+  $('#deleteBtn').on('click', function () {
+    const postId = $(this).data('post-id');
 
+    $.ajax({     // ajax request to delete post
+      url: `/api/post/${postId}`,
+      type: 'DELETE',
+      success: function (data) {
+        console.log('Blog post deleted:', data.message);  // success message
+        $('.blogPostContainer').remove();
+      },
+      error: function (error) {
+        console.error('Error deleting blog post:', error.responseJSON.message);
+      }            // error message for deleting blog posts
+    }); 
+  });
+});
+
+//modal functionality 
+
+$('#deleteBtn').on('click', function () {
+  // show the delete modal
+  $('#deleteModal').modal('show');
+});
+
+// event listener setup for the "Confirm Delete" button
+$('#confirmDelete').on('click', function () {
+
+  const postId = $('#deleteBtn').data('post-id');
+
+
+  // ajax request to delete the blog post
+  $.ajax({
+      url: `/api/posts/${postId}`,
+      type: 'DELETE',
+      success: function (data) {
+          // Handle success, update UI, etc.
+          console.log('Blog post deleted:', data);
+
+          // close the modal after deletion
+          $('#deleteModal').modal('hide');
+    
+      },
+      error: function (error) {
+          // handle error with error message
+          console.error('Error deleting blog post:', error);
+          
+          // close the modal even in case of an error
+          $('#deleteModal').modal('hide');
+      }
+  });
+});
